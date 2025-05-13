3c360cc0b7b7308bfec634a3fcede4ac572b66f6
Tonii18 <ajsanrosal@gmail.com>
2025-05-13 13:48:48 +0200

    Inicio de gestion de almacen Admin

--- src/controladores/OperacionesAdmin.java
+++ src/controladores/OperacionesAdmin.java
@@ -11,30 +11,5 @@ public class OperacionesAdmin {
 	 * En esta clase se desarrollarán todos los métodos para las operaciones de la base de datos de Administrador
 	 */
 	
-	// Metodo para iniciar sesion
 	
-	public static boolean iniciarSesion(Administrador a) {
-		boolean logueado = false;
-		
-		String nombre = a.getNombreAdmin();
-		String clave = a.getClave();
-		
-		String sql = "SELECT * FROM administrador WHERE nombre_admin = ? AND contrasena = ?";
-		
-		try {
-			Connection conn = Conexion.obtener();
-			PreparedStatement ps = conn.prepareStatement(sql);
-			
-			ps.setString(1, nombre);
-			ps.setString(2, clave);
-			
-			logueado = ps.executeQuery().next();
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-		
-		return logueado;
-	}
-
 }
